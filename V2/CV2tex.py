fd1ae694b2db1b741de32825cdaf5995a0f1db71
kwcooper <officialkwade@gmail.com>
2020-02-17 01:52:57 -0800

    ud template

--- V2/CV2tex.py
+++ V2/CV2tex.py
@@ -15,29 +15,30 @@ from jinja2 import Environment, FileSystemLoader
 
 yaml_contents = yaml.load(open("resume.yaml", 'r')) #read data
 
-env = Environment(loader=FileSystemLoader("template"),
+env = Environment(loader=FileSystemLoader("template3"),
   block_start_string='~{',block_end_string='}~',
   variable_start_string='~{{', variable_end_string='}}~')
 
-this_loc = len(open("resume_tex.py", 'r').readlines()) #lets keep it at 42
+this_loc = len(open("CV2tex.py", 'r').readlines()) #lets keep it at 42
 
 def generate():
   body = ""
   for section in yaml_contents['order']: #generate sections 1 by 1
     contents = yaml_contents[section[0]]
     name = section[1].title()
-    body += env.get_template("resume-section.tmpl.tex").render(
+    body += env.get_template("cooperCV-sections.tex").render(
       name = name.upper(),
       contents = contents
     )
   #and then generate the TeX wrapper and fill it with generated sections
-  result = open("result/resume.tex", 'w')
-  result.write(env.get_template("resume.tmpl.tex").render(
-    name = yaml_contents['name'].upper(),
+  result = open("result/cooperCV-res.tex", 'w')
+  result.write(env.get_template("cooperCV.tex").render(
+    name = yaml_contents['name'], #.upper(),
+    lastName = yaml_contents['lastName'], #.upper(),
     email = yaml_contents['email'],
     loc = this_loc, #lines of code in this very script :)
     body = body,
-    today = date.today().strftime("%B %d, %Y") #generation date
+    today = date.today().strftime("%b %d, %Y") #generation date
   ))
   result.close()
 
